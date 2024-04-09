6a4d6e66594210c13b46e812cf13bd1ebd2cfb2e
gawonson <ok466sgw@naver.com>
2024-04-09 19:37:28 +0900

    4월9일스터디

---
+++ gawonson/src/study0409/p334_7.java
@@ -0,0 +1,32 @@
+package study0409;
+ class Parent{
+	public String nation;
+	
+	public Parent() {
+		this("대한민국");
+		System.out.println("Parent() call");
+	}
+	public Parent(String nation) {
+		this.nation=nation;
+		System.out.println("Parent(String nation) call");
+	}
+}
+ class Child extends Parent{
+	public String name;
+	
+	public Child() {
+		this("홍길동");
+		System.out.println("Child() call");
+	}
+	public Child(String name) {
+		this.name=name;
+		System.out.println("Child(String name) call");
+	}
+}
+public class p334_7 {
+	public static void main(String[] args) {
+		Child child=new Child();
+		
+	}
+
+}
