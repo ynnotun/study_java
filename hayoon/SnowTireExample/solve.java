d2837d8db2b6aaa5503eb1d08606faf2f9ceaaf0
hayooniiiiii <140838571+hayooniiiiii@users.noreply.github.com>
2024-04-11 12:48:54 +0900

    Create solve.java

---
+++ hayoon/SnowTireExample/solve.java
@@ -0,0 +1,10 @@
+ package day0411;
+
+public class SnowTireExample {
+	public static void main(String[] args) {
+		SnowTire snowTire=new SnowTire();
+		Tire tire=snowTire;
+		snowTire.run();
+		tire.run();
+	}
+}
