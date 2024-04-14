d9d4b965fee52a3805d2bc0e355c7c4c019125ec
JUYONG0133 <qwer99633@naver.com>
2024-04-14 20:17:31 +0900

    8장 7번 수정

--- JUYONG0133/src/stu/day0415/page390.java
+++ JUYONG0133/src/stu/day0415/page390.java
@@ -1,15 +1,18 @@
 package stu.day0415;
 
-//page390 인터페이스
+//DataAccessObject 인터페이스 선언
+
 interface DataAccessObject{
     void select();
     void insert();
     void update();
     void delete();
-
+// select, insert, update(), delete() 메소드 선언
 }
-//oracledao 클래스 선언
+//oracledao 클래스 선언 이 클래스는 dataaccessobject 인터페이스를구현
 class OracleDao implements DataAccessObject {
+
+    //oracle 데이터베이스에서 각, 검색, 삽입, 수정, 삭제 작업을 수행하는 기능 구현
     @Override
     public void select() {
         System.out.println("oracledb에서 검색");
@@ -30,8 +33,9 @@ class OracleDao implements DataAccessObject {
     }
 }
 
-//mysqldao 클래스 선언
+//mysqldao 클래스 선언 선언 이 클래스는 dataaccessobject 인터페이스를구현
 class MysqlDao implements DataAccessObject{
+    //mysql 데이터베이스에서 각, 검색, 삽입, 수정, 삭제 작업을 수행하는 기능 구현
     @Override
     public void select() {
         System.out.println("mysqldb에서 검색");
@@ -53,6 +57,8 @@ class MysqlDao implements DataAccessObject{
     }
 }
 public class page390 {
+
+    // dawork라는 정적 메소드를 선언, dataaccessobject 타입의 객체를 매개변수로 받음
     public static void dbWork(DataAccessObject dao){
         dao.select();
         dao.insert();
