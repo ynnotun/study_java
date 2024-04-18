605441026dae2ecba9a35d715aa2f03436034d20
hayooniiiiii <140838571+hayooniiiiii@users.noreply.github.com>
2024-04-18 16:49:17 +0900

    Create solve.java

---
+++ hayoon/LoginExample/solve.java
@@ -0,0 +1,38 @@
+package day0418;
+
+public class LoginExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			login("white","12345");
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			login("blue","54321");
+			
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+	
+	
+	public static void login(String id, String password) throws NotExistIDException,WrongPasswordException {
+		if(!id.equals("blue")) {
+			throw new NotExistIDException("아이디가 존재하지 않습니다.");
+		}
+		if(!password.equals("12345")) {
+			throw new WrongPasswordException("패스워드가 틀립니다.");
+			
+		}
+	}
+
+
+
+
+}
